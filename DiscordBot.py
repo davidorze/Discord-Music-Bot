fe9efedcfe79e7edb57fca7f364a62ee2df49a55
David Orze <52167789+davidorze@users.noreply.github.com>
2022-01-27 16:47:34 -0300

    teste branches v0.0.1

--- DiscordBot.py
+++ DiscordBot.py
@@ -459,8 +459,7 @@ async def lyrics(ctx):
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     
-    #checking = asyncio.new_event_loop()
-    #asyncio.run_coroutine_threadsafe(checkIfAlone())
+
 
 #@client.event
 #async def on_message(message):
